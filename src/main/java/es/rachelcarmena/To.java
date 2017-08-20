d8be686cd257491bb3b7721713dae9e1feb3bf63
Rachel <raquel.moreno.carmena@gmail.com>
2017-08-20 21:36:06 +0200

    Red tests: removing equals and hashCode. Using reflection to compare

--- src/main/java/es/rachelcarmena/To.java
+++ src/main/java/es/rachelcarmena/To.java
@@ -13,22 +13,4 @@ public class To {
         this.param2 = param2;
         this.param3 = param3;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof To)) return false;
-
-        To toClass = (To) o;
-
-        if (!param1.equals(toClass.param1)) return false;
-        return param2.equals(toClass.param2);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = param1.hashCode();
-        result = 31 * result + param2.hashCode();
-        return result;
-    }
 }
--- src/test/java/es/rachelcarmena/AdapterTest.java
+++ src/test/java/es/rachelcarmena/AdapterTest.java
@@ -1,11 +1,11 @@
 package es.rachelcarmena;
 
 import org.junit.Test;
+import org.mockito.internal.matchers.apachecommons.ReflectionEquals;
 
 import static es.rachelcarmena.To.status.BLACK;
 import static es.rachelcarmena.To.status.WHITE;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class AdapterTest {
 
@@ -13,15 +13,17 @@ public class AdapterTest {
 	public void convert_from_object_to_another_one() {
 		From from = new From(12, 24);
 		To to = new To("12", "24", BLACK);
+		ReflectionEquals eqTo = new ReflectionEquals(to);
 
-		assertThat(Adapter.adapt(from), is(to));
+		assertTrue(eqTo.matches(Adapter.adapt(from)));
 	}
 
 	@Test
 	public void convert_from_object_to_another_one_with_an_additional_property() {
 		From from = new From(12, 6);
 		To to = new To("12", "6", WHITE);
+		ReflectionEquals eqTo = new ReflectionEquals(to);
 
-		assertThat(Adapter.adapt(from), is(to));
+		assertTrue(eqTo.matches(Adapter.adapt(from)));
 	}
 }
