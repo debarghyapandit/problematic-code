53ccd554046f54a80555ff1c8ad5ec658120edcb
Rachel <raquel.moreno.carmena@gmail.com>
2017-08-21 20:35:20 +0200

    Boundaries included

--- src/test/java/es/rachelcarmena/AdapterTest.java
+++ src/test/java/es/rachelcarmena/AdapterTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertTrue;
 public class AdapterTest {
 
 	@Test
-	public void convert_from_object_to_another_one() {
+	public void convert_from_object_to_another_one_when_param1_less_than_param2() {
 		From from = new From(12, 24);
 		To to = new To("12", "24", BLACK);
 		ReflectionEquals eqTo = new ReflectionEquals(to);
@@ -19,7 +19,16 @@ public class AdapterTest {
 	}
 
 	@Test
-	public void convert_from_object_to_another_one_with_an_additional_property() {
+	public void convert_from_object_to_another_one_when_equals_params() {
+		From from = new From(12, 12);
+		To to = new To("12", "12", WHITE);
+		ReflectionEquals eqTo = new ReflectionEquals(to);
+
+		assertTrue(eqTo.matches(Adapter.adapt(from)));
+	}
+
+	@Test
+	public void convert_from_object_to_another_one_when_param1_greater_than_param2() {
 		From from = new From(12, 6);
 		To to = new To("12", "6", WHITE);
 		ReflectionEquals eqTo = new ReflectionEquals(to);
