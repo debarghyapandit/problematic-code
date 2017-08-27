0c5feef71c2d5807049939fc1a1efeb53023a4d9
Rachel <raquel.moreno.carmena@gmail.com>
2017-08-27 21:31:09 +0200

    Simplify code

--- src/main/java/es/rachelcarmena/Adapter.java
+++ src/main/java/es/rachelcarmena/Adapter.java
@@ -1,5 +1,7 @@
 package es.rachelcarmena;
 
+import es.rachelcarmena.To.status;
+
 import static es.rachelcarmena.To.status.BLACK;
 import static es.rachelcarmena.To.status.WHITE;
 
@@ -8,7 +10,7 @@ public class Adapter {
     public static To adapt(From from) {
         String arg1 = String.valueOf(from.getParam1());
         String arg2 = String.valueOf(from.getParam2());
-        To.status arg3 = ((from.getParam1() < from.getParam2())? BLACK: WHITE);
+        status arg3 = ((from.getParam1() < from.getParam2())? BLACK: WHITE);
 
         return new To(arg1, arg2, arg3);
     }
