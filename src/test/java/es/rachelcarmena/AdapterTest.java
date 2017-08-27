0c7c6124b7e55305e9e4aa071bbe2bdc4be907b6
Rachel <raquel.moreno.carmena@gmail.com>
2017-08-27 21:31:38 +0200

    Replacing ReflectionEquals from Mockito with EqualsBuilder from Apache Commons Lang

--- src/test/java/es/rachelcarmena/AdapterTest.java
+++ src/test/java/es/rachelcarmena/AdapterTest.java
@@ -1,11 +1,11 @@
 package es.rachelcarmena;
 
 import org.junit.Test;
-import org.mockito.internal.matchers.apachecommons.ReflectionEquals;
 
 import static es.rachelcarmena.To.status.BLACK;
 import static es.rachelcarmena.To.status.WHITE;
 import static org.junit.Assert.assertTrue;
+import static org.apache.commons.lang3.builder.EqualsBuilder.reflectionEquals;
 
 public class AdapterTest {
 
@@ -13,26 +13,23 @@ public class AdapterTest {
 	public void convert_from_object_to_another_one_when_param1_less_than_param2() {
 		From from = new From(12, 24);
 		To to = new To("12", "24", BLACK);
-		ReflectionEquals eqTo = new ReflectionEquals(to);
 
-		assertTrue(eqTo.matches(Adapter.adapt(from)));
+		assertTrue(reflectionEquals(to, Adapter.adapt(from)));
 	}
 
 	@Test
 	public void convert_from_object_to_another_one_when_equals_params() {
 		From from = new From(12, 12);
 		To to = new To("12", "12", WHITE);
-		ReflectionEquals eqTo = new ReflectionEquals(to);
 
-		assertTrue(eqTo.matches(Adapter.adapt(from)));
+		assertTrue(reflectionEquals(to, Adapter.adapt(from)));
 	}
 
 	@Test
 	public void convert_from_object_to_another_one_when_param1_greater_than_param2() {
 		From from = new From(12, 6);
 		To to = new To("12", "6", WHITE);
-		ReflectionEquals eqTo = new ReflectionEquals(to);
 
-		assertTrue(eqTo.matches(Adapter.adapt(from)));
+		assertTrue(reflectionEquals(to, Adapter.adapt(from)));
 	}
 }
