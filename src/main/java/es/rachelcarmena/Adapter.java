64db4f48130b4d4622c10f3858ac3575b39eaa3e
Rachel <raquel.moreno.carmena@gmail.com>
2018-12-09 11:40:45 +0100

    Removing unnecessary difference

--- src/main/java/es/rachelcarmena/Adapter.java
+++ src/main/java/es/rachelcarmena/Adapter.java
@@ -5,12 +5,11 @@ import es.rachelcarmena.To.status;
 import static es.rachelcarmena.To.status.BLACK;
 import static es.rachelcarmena.To.status.WHITE;
 
-public class    Adapter {
+public class Adapter {
 
     public static To adapt(From from) {
         String arg1 = String.valueOf(from.getParam1());
         String arg2 = String.valueOf(from.getParam2());
-
         status arg3 = ((from.getParam1() < from.getParam2())? BLACK: WHITE);
 
         return new To(arg1, arg2, arg3);
